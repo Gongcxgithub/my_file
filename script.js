75f4f5d469abda83ac97a28948ba36329a9953c8
Gongcxgithub <69497500+Gongcxgithub@users.noreply.github.com>
2021-02-01 17:13:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+        document.getElementById("btn").onclick = function myFonction () {
+            var utext = document.getElementById("area").value;
+            console.log(utext);
+            document.write(utext);
+            
+        }
+        
